1f32892914cee822f80504662440b11500e2d88a
Paul Molluzzo <paul@molluzzo.com>
2017-01-10 13:35:27 -0500

    Basic header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const wHeight = function() { return window.innerHeight; }
+const wWidth = function() { return window.innerHeight; }
+
+document.querySelector('header').style.height = wHeight() + 'px';

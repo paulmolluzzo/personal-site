ecab4baff62d7c4ee80d317f06c4f03be2f8c7b7
Paul Molluzzo <paul@molluzzo.com>
2017-01-13 10:55:03 -0500

    Add hardcoded work list

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,24 @@
 
 const wHeight = function() { return window.innerHeight; }
 const wWidth = function() { return window.innerHeight; }
+const projects = document.getElementsByClassName('project');
 
 function resizeHeader() {
   document.querySelector('header').style.height = wHeight() + 'px';
 }
 
+function resizeProjectList() {
+  if (projects.length) {
+    const tempWidth = ((projects[0].offsetWidth * 0.66) - 1).toFixed(0);
+
+    for (var i=0; i < projects.length; i++) {
+      projects[i].style.height = tempWidth + 'px';
+    }
+  }
+}
+
 window.addEventListener('resize', resizeHeader, false);
+window.addEventListener('resize', resizeProjectList, false);
 
 resizeHeader();
+resizeProjectList();

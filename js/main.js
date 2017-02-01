435719fa45be4e8b29b975b21c26d1789f992e09
Paul Molluzzo <paul@molluzzo.com>
2017-02-01 11:30:26 -0500

    Only reset height of header if window width changes

--- js/main.js
+++ js/main.js
@@ -4,14 +4,37 @@ const wHeight = function () {
   return window.innerHeight;
 };
 
+const wWidth = function () {
+  return window.innerWidth;
+};
+
+let lastWidth = wWidth();
+
+const updateWindowWidth = function () {
+  lastWidth = wWidth();
+};
+
+const newWidth = function () {
+  return lastWidth !== wWidth();
+};
+
 const projects = document.getElementsByClassName('project');
 
 function resizeHeader() {
-  document.querySelector('header').style.height = wHeight() + 'px';
+  // if there is no height set or the window has a new width set the height
+  if (!document.querySelector('header').style.height || newWidth()) {
+    document.querySelector('header').style.height = wHeight() + 'px';
+
+    // update window width to test for future resize changes
+    updateWindowWidth();
+  }
 }
 
 function resizeProjectList() {
-  if (projects.length > 0) {
+  if (projects.length > 0 && newWidth()) {
+    // update window width to test for future resize changes
+    updateWindowWidth();
+
     const tempWidth = ((projects[0].offsetWidth * 0.66) - 1).toFixed(0);
 
     for (var i = 0; i < projects.length; i++) {

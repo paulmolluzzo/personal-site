4f2428b842a1b63640d4c64efbfa5a7af540ff69
Paul Molluzzo <paul@molluzzo.com>
2017-02-06 13:18:19 -0500

    Make project heights a fixed height

--- js/main.js
+++ js/main.js
@@ -18,8 +18,6 @@ const newWidth = function () {
   return lastWidth !== wWidth();
 };
 
-const projects = document.getElementsByClassName('project');
-
 function resizeHeader() {
   // if there is no height set or the window has a new width set the height
   if (!document.querySelector('header').style.height || newWidth()) {
@@ -27,22 +25,7 @@ function resizeHeader() {
   }
 }
 
-function resizeProjectList() {
-  if (projects.length > 0) {
-    if (!projects[0].style.height || newWidth()) {
-      const tempWidth = ((projects[0].offsetWidth * 0.66) - 1).toFixed(0);
-
-      // set height on all projects
-      for (var i = 0; i < projects.length; i++) {
-        projects[i].style.height = tempWidth + 'px';
-      }
-    }
-  }
-}
-
 window.addEventListener('resize', resizeHeader, false);
-window.addEventListener('resize', resizeProjectList, false);
 window.addEventListener('resize', updateWindowWidth, false);
 
 resizeHeader();
-resizeProjectList();
